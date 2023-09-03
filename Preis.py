e8cd92ce6625bbf5684fd83787645836f5620e21
Misterhh-creator <wais_finanz@yahoo.de>
2023-09-03 14:48:03 +0200

    time.sleep Parameter

--- Preis.py
+++ Preis.py
@@ -3,27 +3,31 @@ import time
 from bs4 import BeautifulSoup
 import json
 
+Preisdaten = {}
+Anzahl=10
+#def scrapebitcoin():
+    # def scrapebitcoin(Anzahl: int):
 
-Anzahl = 10 
-Preisdaten={}
+def timesleep():
+     time.sleep(5)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/36.0'}
 
 URL = "https://finance.yahoo.com/quote/BTC-USD"
 
 while Anzahl>0:
-    Anzahl -= 1
+        Anzahl -= 1
 
-    response = requests.get(URL, headers=headers)
+        response = requests.get(URL, headers=headers)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    for x in soup.find_all(name="fin-streamer", attrs={"data-field": "regularMarketPrice", "data-symbol":"BTC-USD"}): #data-field + data-symbol
-        #print(time.asctime()+': ' +x.text)
-        Preisdaten[time.asctime()] = x.text
-        print(Preisdaten)
-    
-    time.sleep(5)
+        for x in soup.find_all(name="fin-streamer", attrs={"data-field": "regularMarketPrice", "data-symbol":"BTC-USD"}): #data-field + data-symbol
+            #print(time.asctime()+': ' +x.text)
+            Preisdaten[time.asctime()] = x.text
+            print(Preisdaten)
+        
+        #time.sleep(5)
 
 json = json.dumps(Preisdaten)
 
@@ -31,4 +35,6 @@ f = open("Bitcoin.json","w")
 f.write(json)
 f.close()
 
+#scrapebitcoin(2)
+timesleep()
 
