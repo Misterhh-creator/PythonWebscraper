a948c1116a4682e5570dd63940a930fa5a96cdda
Misterhh-creator <wais_finanz@yahoo.de>
2023-09-21 19:06:40 +0200

    Graphische Darstellung Bitcoin Kursentwicklung

--- Preis.py
+++ Preis.py
@@ -1,7 +1,22 @@
 import requests
+import datetime
 import time
 from bs4 import BeautifulSoup
 import json
+import matplotlib.pyplot as plt
+import numpy as np
+# 1. Git commit
+# 2. Suche aus der Bitcoin Preisliste den max und den min. Wert
+# 3. Passe in Zeile 17 die Reichweite des Plots an den min und den max Wert an
+
+def darstellen_als_treppenplot(x: list, y: list):
+      plt.style.use('_mpl-gallery') 
+      fig, ax = plt.subplots() 
+      ax.plot(x, y, linewidth=2.0)
+      ax.set(xlim=(0, 220), xticks=np.arange(0, 220), 
+             ylim=(27100, 27300), yticks=np.arange(27100, 27300)) #zwischen 0 und 8 in Schritten 1-7
+      plt.show()
+    
 
 Preisdaten = {}
 Anzahl=10
@@ -9,7 +24,7 @@ Anzahl=10
     # def scrapebitcoin(Anzahl: int):
 
 def timesleep():
-     time.sleep(5)
+     time.sleep(10)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/36.0'}
 
@@ -24,10 +39,13 @@ while Anzahl>0:
 
         for x in soup.find_all(name="fin-streamer", attrs={"data-field": "regularMarketPrice", "data-symbol":"BTC-USD"}): #data-field + data-symbol
             #print(time.asctime()+': ' +x.text)
-            Preisdaten[time.asctime()] = x.text
+            Preisdaten[int(datetime.datetime.utcnow().timestamp()) % 200] = float(x.text.replace(",",""))
             print(Preisdaten)
         
-        #time.sleep(5)
+        timesleep()
+
+x = list(Preisdaten.keys())
+y = list(Preisdaten.values())
 
 json = json.dumps(Preisdaten)
 
@@ -35,6 +53,10 @@ f = open("Bitcoin.json","w")
 f.write(json)
 f.close()
 
-#scrapebitcoin(2)
-timesleep()
+darstellen_als_treppenplot(x, y)
+
+
+
+
+
 
